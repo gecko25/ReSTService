008a36b5df3b6a1263ed1f8777ff624a3bd8b68c
gecko25 <sara.tankard@gmail.com>
2015-08-04 12:08:29 -0400

    added book model to translate data from mongodb to json using schema

---
+++ models/bookModel.js
@@ -0,0 +1,21 @@
+//Mongoose translates data from mongodb using a model
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var bookModel = new Schema({
+    title: {
+        type: String
+    },
+    author: {type: String},
+    genre: {type: String},
+    read: {type: Boolean, default:false}
+});
+
+//Were going to load this model into mongoose 
+//& call it 'Book'
+
+//We have a new model (a new schema) call Book
+//Lets return this book model so that app.js we 
+//can have an instance of that book model
+module.exports= mongoose.model('Book', bookModel);
