55e074e1146589f902067112ce773860bd2a3807
gecko25 <sara.tankard@gmail.com>
2015-08-07 11:24:33 -0400

    cleaning some comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 /**
- *The file that gulp looks to for configuration.
- *When gulp runs, it is going to look for this file.
- **/
-
-/******
-nodemon - a plugin(?) that automatically
+ ** The file that gulp looks to for configuration.
+ ** When gulp runs, it is going to look for this file.
+  
+  nodemon - a plugin that automatically
           restarts server when changes to files are made
-gulp    - "streaming build system" (like ant I think)          
-******/
+  gulp    - "streaming build system" (like ant I think)          
+**/
 
 var gulp    = require('gulp') 
 	nodemon = require('gulp-nodemon');  
@@ -16,15 +14,15 @@ var gulp    = require('gulp')
 gulp.task('default', function setupNodemon(){
 	//nodemon is configured by a JSON object
 	nodemon({
-		script: 'app.js',  //what script will it run
-		ext: 'js',		  //what extentions to watch for
+		script: 'app.js',  //what is nodemon going to run on server restart? 
+		ext: 'js',		   //what extentions to watch for changes (i.e when .js file changes, restart!)
 		env: {
 			PORT:8000	  //nodemon lets us setup our environment here (process.env.POR)
 		},
-		ignore: ['/.node_modules/**']  //so future installs dont get confused?
+		ignore: ['/.node_modules/**']  //do NOT restart when these files change
 	})
 	.on('restart', function restart(){
-		console.log("Restarting..");
+		console.log("Restarting..");  //do something on restart
 	});
 })
 
@@ -33,7 +31,7 @@ gulp.task('default', function setupNodemon(){
 	We created an instance of nodeman -->var nodeman = require('nodeman')
 	Then called a method on that instance and passed it a config obj as a param
 	nodeman({configObj}).on(somestuff)
-		//Whats going on behind scenes here? -- this is a constructor function Im guessing)
-		//source code for this doesn't make tons of sense to me, so not sure --> https://github.com/remy/nodemon/tree/master/lib
+-- Whats going on behind scenes here? -- this is a constructor function Im guessing?
+-- https://github.com/remy/nodemon/tree/master/lib
 */	
 	
