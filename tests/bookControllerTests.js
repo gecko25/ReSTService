294ec0b9be4217489d60d2423ad0949ced81fa38
gecko25 <sara.tankard@gmail.com>
2015-08-08 15:00:34 -0400

    create bdd files

---
+++ tests/bookControllerTests.js
@@ -0,0 +1,15 @@
+var should = require('should'),
+	sinon = require('sinon');
+
+
+describe('Book Controller Tests:', function()){
+	describe('Post', function(){
+		//testing Post method of book contraoller
+		it('should not allow empty title on post', function(){
+			var Book = function(book){this.save = function(){}};
+		
+			var 
+
+		});
+	})
+}
